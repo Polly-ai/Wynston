a75e163f0ce9581c21a7818d779c14e16f5c949c
Polly-ai <67654027+Polly-ai@users.noreply.github.com>
2022-10-29 15:03:30 +0800

    Update Apowersoft.js

--- Surge/Script/Apowersoft.js
+++ Surge/Script/Apowersoft.js
@@ -1,7 +1,6 @@
 [rewrite_local]
 
 # ～ 傲软抠图解锁会员权限（2022-09-13）@ddgksf2013
-
 https?:\/\/.*\.aoscdn\.com\/base\/vip\/client\/authorizations$ url script-response-body https://gitlab.com/ddgksf2013/Cuttlefish/-/raw/master/Crack/apowersoft.js
 
 [mitm] 
@@ -10,50 +9,29 @@ hostname=*.aoscdn.com
 
 ***********************************/
 
+
+
 var cuttlefish ={"warning":"本腳本僅供學習交流使用，禁止轉載售賣","tgchannel":"https://t.me/ddgksf2021","feedback":"https://t.me/ddgksf2013_bot"};
-
 var ddgksf2013 = {
-
   "status" : 200,
-
   "message" : "success",
-
   "data" : {
-
     "expired_at" : 4045798296,
-
     "is_activated" : 1,
-
     "is_lifetime" : 1,
-
     "expire_time" : "2099-01-01 00:00:00",
-
     "device_id" : 600150864,
-
     "period_type" : "active",
-
     "remain_days" : 99999,
-
     "product_id" : 369,
-
     "has_present" : 0,
-
     "allowed_device_count" : 1,
-
     "has_buy_extend" : 0,
-
     "will_expire" : 0,
-
     "license_type" : "premium",
-
     "begin_activated_time" : 1645798296,
-
     "durations" : 0,
-
     "vip_special" : 1
-
   }
-
 };
-
 $done({body: JSON.stringify(ddgksf2013)});
