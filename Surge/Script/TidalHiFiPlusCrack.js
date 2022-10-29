81c40f81aa73aacaa66da3e7bc1b4e9e3440ac68
Polly-ai <67654027+Polly-ai@users.noreply.github.com>
2022-10-29 15:13:20 +0800

    Create TidalHiFiPlusCrack.js

---
+++ Surge/Script/TidalHiFiPlusCrack.js
@@ -0,0 +1,26 @@
+[rewrite_local]
+# > TIDAL解锁HiFi Plus
+^https?:\/\/api\.tidal\.com\/v1\/users\/\d+\/subscription.+ url script-response-body https://raw.githubusercontent.com/Polly-ai/Wynston/main/Surge/Script/TIDALHiFiPlusCrack.js
+^https?:\/\/api\.tidal\.com\/v1\/tracks/\d+\/playbackinfopostpaywall.+ url script-analyze-echo-response https://raw.githubusercontent.com/Polly-ai/Wynston/main/Surge/Script/TidalHiFiPlusCrack.js
+
+[mitm] 
+hostname = api.tidal.com
+
+*******************************/
+
+$done({
+    body: JSON.stringify({
+        "status": "ACTIVE",
+        "canGetTrial": false,
+        "startDate": "2019-09-28T09:09:09.000+0000",
+        "subscription": {
+            "type": "PREMIUM_PLUS",
+            "offlineGracePeriod": 999999
+        },
+        "premiumAccess": true,
+        "paymentType": "ADYEN_CREDIT_CARD",
+        "paymentOverdue": false,
+        "highestSoundQuality": "HI_RES",
+        "validUntil": "2999-09-28T09:09:09.000+0000"
+    })
+});
