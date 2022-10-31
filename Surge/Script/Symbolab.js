ef7143978d9622384213df8475464b94ba7fdccb
Polly-ai <67654027+Polly-ai@users.noreply.github.com>
2022-10-31 13:19:42 +0800

    Create Symbolab.js

---
+++ Surge/Script/Symbolab.js
@@ -0,0 +1,24 @@
+#!name=Symbolab
+#!desc=解锁Premium
+
+
+
+[Scrpit]
+# > Symbolab 高级功能解锁 （需登录）
+Symbolab = type=http-response,pattern=^https?:\/\/scibug\.com\/appleSubscriptionValidate$,requires-body=1,script-path=https://gitlab.com/ddgksf2013/Cuttlefish/-/raw/master/Crack/symbolab.js
+
+[MITM]
+hostname = %APPEND% scibug.com
+
+
+
+
+
+
+
+
+let obj = JSON.parse($response.body);
+
+obj= {"valid":true,"hasUserConsumedAppleFreeTrial":false,"isCurrentlyInFreeTrial":false,"newlyAssociated":false,"membership":{"isCurrentlyInFreeTrial":false,"valid":true,"hasUserConsumedAppleFreeTrial":false,"newlyAssociated":false}}
+
+$done({body: JSON.stringify(obj)});
