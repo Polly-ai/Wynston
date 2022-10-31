98de626e70f6da192ecd9087c77e82c264a38d22
Polly-ai <67654027+Polly-ai@users.noreply.github.com>
2022-10-31 13:32:09 +0800

    Update Apowersoft.js

--- Surge/Script/Apowersoft.js
+++ Surge/Script/Apowersoft.js
@@ -1,7 +1,7 @@
 [rewrite_local]
 
 # ～ 傲软抠图解锁会员权限（2022-09-13）@ddgksf2013
-https?:\/\/.*\.aoscdn\.com\/base\/vip\/client\/authorizations$ url script-response-body https://gitlab.com/ddgksf2013/Cuttlefish/-/raw/master/Crack/apowersoft.js
+https?:\/\/.*\.aoscdn\.com\/base\/vip\/client\/authorizations$ url script-response-body https://gitlab.com/ddgksf2013/Cuttlefish/-/raw/master/Crack/Apowersoft.js
 
 [mitm] 
 
